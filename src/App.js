5595ee93b564bfd285d5401636e4073f8f10e3fe
et357 <peter001019@naver.com>
2024-11-26 09:38:08 +0900

    [feat] Login Page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import logo from "./logo.svg";
 import "./App.css";
-import SignIn from "./page/SignIn/SignIn";
+import SignIn from "./page/SignIn";
+import Login from "./page/Login";
 
 function App() {
-  return <SignIn />;
+  return <Login />;
 }
 
 export default App;
--- src/page/SignIn.js
+++ src/page/SignIn.js
@@ -1,6 +1,6 @@
 import React from "react";
 import styled from "styled-components";
-import Header from "../../component/Header/Header";
+import Header from "../component/Header/Header";
 
 const SignIn = () => {
   return (
